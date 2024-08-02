2400c4fd6041aa10a33d43a708e2e659b02ab51b
Nathan Seymour <nathan@seymour.global>
2024-08-02 13:15:49 -0500

    initial commit - separation from unlogic

---
+++ README.md
@@ -0,0 +1,7 @@
+# buffalo
+> *buffalo: to bewilder, baffle (also: **bamboozle**)* [2024 Merriam-Webster Dictionary]
+
+Bewilder, baffle (even bamboozle!) your coworkers with `buffalo`, a pure C++23 header-only SLR parser generator with 
+ `yacc/bison`-like syntax.
+
+## Examples
---
+++ test/buffalo.test.cpp
@@ -0,0 +1,63 @@
+#include <gtest/gtest.h>
+#include <variant>
+#include <buffalo/buffalo.h>
+
+enum class TerminalType
+{
+    NUMBER,
+    IDENTIFIER,
+    KEYWORD,
+    OPERATOR,
+    SYMBOL,
+    END,
+};
+
+typedef std::variant<double, std::string> ValueType;
+
+buffalo::Tokenizer<TerminalType, ValueType> tokenizer;
+
+buffalo::DefineTerminal<TerminalType, ValueType, R"(\-?\d+(\.\d+)?)", double> NUMBER(tokenizer, TerminalType::NUMBER, [](auto tok)
+{
+    return std::stod(std::string(tok.raw));
+});
+
+buffalo::DefineTerminal<TerminalType, ValueType, R"(\+|\-|\*|\/)", std::string> OPERATOR(tokenizer, TerminalType::OPERATOR, [](auto tok)
+{
+    return std::string(tok.raw);
+});
+
+buffalo::DefineTerminal<TerminalType, ValueType, R"([a-zA-Z\d]+)", std::string> IDENTIFIER(tokenizer, TerminalType::IDENTIFIER, [](auto tok)
+{
+    return std::string(tok.raw);
+});
+
+buffalo::DefineTerminal<TerminalType, ValueType, R"(\s*$)", double> END(tokenizer, TerminalType::END, [](auto tok) {
+    return 0.0;
+});
+
+buffalo::NonTerminal<TerminalType, ValueType> expression
+    = (expression + OPERATOR + expression)<=>[](auto &$)
+    {
+        return std::get<double>($[0]) + std::get<double>($[2]);
+    }
+    | buffalo::ProductionRule(NUMBER) <=> [](auto &$)
+    {
+        return std::get<double>($[0]);
+    }
+    | buffalo::ProductionRule(IDENTIFIER) <=> [](auto &$)
+    {
+        return 0.0;
+    };
+
+buffalo::NonTerminal<TerminalType, ValueType> program
+    = (expression + END)<=>[](auto &$)
+    {
+        return std::get<double>($[0]);
+    };
+
+TEST(Parser, BasicParsing)
+{
+    buffalo::Parser<TerminalType, ValueType> p(tokenizer, program);
+
+    //ASSERT_EQ(81.5, std::get<double>(result));
+}
